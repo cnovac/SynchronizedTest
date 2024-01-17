5c6bc6c45954cd0855a6705f5c2ddffa5ac62087
Cristian Novac <cristian.novac@ness.com>
2024-01-18 01:38:47 +0200

    working.changed the shared variable from int to Integer, so that more differences appear between testing with/without Synchronized block

--- src/MyInteger.java
+++ src/MyInteger.java
@@ -1,6 +1,6 @@
 public class MyInteger {
     private static MyInteger single_instance = null;
-    public static int a = 0;//can make this volatile, but no need if Synchronized block is used
+    public static Integer a = 0;//can make this volatile, but no need if Synchronized block is used
     //Synchronized block will make sure the value is visible/propagated between all threads.
     public static synchronized MyInteger getInstance()
     {
