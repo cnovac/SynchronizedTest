23e4d2004dd8d9e5dd031ffa1becec9389acf65d
Cristian Novac <cristian.novac@ness.com>
2024-01-18 00:58:39 +0200

    working.added processing time

--- src/MyInteger.java
+++ src/MyInteger.java
@@ -1,6 +1,7 @@
 public class MyInteger {
     private static MyInteger single_instance = null;
-    public static volatile int a = 0;//make this volatile
+    public static int a = 0;//can make this volatile, but no need if Synchronized block is used
+    //Synchronized block will make sure the value is visible/propagated between all threads.
     public static synchronized MyInteger getInstance()
     {
         if (single_instance == null)
--- src/TestSyncVolatile.java
+++ src/TestSyncVolatile.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestSyncVolatile {
-    private static final int NO_OF_THREADS = 10000;
+    private static final int NO_OF_THREADS = 100000;
     public static List<ThreadTest> threadTestList = new ArrayList<>();
     public static void main( String [] args){
         System.out.println("Synchronized, Volatile TEST !!!!!");
+        System.out.println("Each thread adds 10 to the total(a variable) => Total should be equal to No of threads * 10 !!!!!");
         long start = System.nanoTime();
 
         for(int i = 0; i < NO_OF_THREADS; i++) {
